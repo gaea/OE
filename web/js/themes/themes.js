ee25de5c6e0a8d3c82ef26579b1a535b5e6954cb
gustavo <gaea@gaea-desktop.(none)>
2011-04-01 08:02:15 -0500

    admin themes

---
+++ web/js/themes/themes.js
@@ -0,0 +1,182 @@
+var gestionar_estudiantes_panel = new Ext.Panel({
+	layout:'border',
+	height:window.innerHeight-112,
+	monitorResize: true,
+	tbar:[
+		{
+			xtype:'buttongroup',
+			title:"Apariencia",
+			items:[
+				{
+					text:"access",
+					iconAlign:'top',
+					iconCls:'agregar_estudiante24',
+					scale:'medium',
+					handler:function(){
+						
+					}
+				},
+				{
+					text:"black",
+					iconAlign:'top',
+					iconCls:'modificar_estudiante24',
+					scale:'medium',
+					handler:function(){
+						
+					}
+				},
+				{
+					text:"blue",
+					iconAlign:'top',
+					iconCls:'habilitar_estudiante24',
+					scale:'medium',
+					handler:function(){
+						
+					}
+				},
+				{
+					text:"chocolate",
+					iconAlign:'top',
+					iconCls:'desabilitar_estudiante24',
+					scale:'medium',
+					handler:function(){
+						
+					}
+				},
+				{
+					text:"gray",
+					iconAlign:'top',
+					iconCls:'desabilitar_estudiante24',
+					scale:'medium',
+					handler:function(){
+						
+					}
+				},
+				{
+					text:"green",
+					iconAlign:'top',
+					iconCls:'desabilitar_estudiante24',
+					scale:'medium',
+					handler:function(){
+						
+					}
+				},
+				{
+					text:"indigo",
+					iconAlign:'top',
+					iconCls:'desabilitar_estudiante24',
+					scale:'medium',
+					handler:function(){
+						
+					}
+				},
+				{
+					text:"midnight",
+					iconAlign:'top',
+					iconCls:'desabilitar_estudiante24',
+					scale:'medium',
+					handler:function(){
+						
+					}
+				},
+				{
+					text:"olive",
+					iconAlign:'top',
+					iconCls:'desabilitar_estudiante24',
+					scale:'medium',
+					handler:function(){
+						
+					}
+				},
+				{
+					text:"peppermint",
+					iconAlign:'top',
+					iconCls:'desabilitar_estudiante24',
+					scale:'medium',
+					handler:function(){
+						
+					}
+				},
+				{
+					text:"purple",
+					iconAlign:'top',
+					iconCls:'desabilitar_estudiante24',
+					scale:'medium',
+					handler:function(){
+						
+					}
+				},
+				{
+					text:"quizz",
+					iconAlign:'top',
+					iconCls:'desabilitar_estudiante24',
+					scale:'medium',
+					handler:function(){
+						
+					}
+				},
+				{
+					text:"red",
+					iconAlign:'top',
+					iconCls:'desabilitar_estudiante24',
+					scale:'medium',
+					handler:function(){
+						
+					}
+				},
+				{
+					text:"silverCherry",
+					iconAlign:'top',
+					iconCls:'desabilitar_estudiante24',
+					scale:'medium',
+					handler:function(){
+						
+					}
+				},
+				{
+					text:"slate",
+					iconAlign:'top',
+					iconCls:'desabilitar_estudiante24',
+					scale:'medium',
+					handler:function(){
+						
+					}
+				},
+				{
+					text:"slickness",
+					iconAlign:'top',
+					iconCls:'desabilitar_estudiante24',
+					scale:'medium',
+					handler:function(){
+						
+					}
+				},
+				{
+					text:"univalle",
+					iconAlign:'top',
+					iconCls:'desabilitar_estudiante24',
+					scale:'medium',
+					handler:function(){
+						
+					}
+				},
+				{
+					text:"vista",
+					iconAlign:'top',
+					iconCls:'desabilitar_estudiante24',
+					scale:'medium',
+					handler:function(){
+						
+					}
+				}
+			]
+		}
+	],
+	items:[
+		{
+			xtype:'panel',
+			
+		}
+	],
+	renderTo:'div_form_themes'
+});
